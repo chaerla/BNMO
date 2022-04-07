f353c1e05e782759ac4b06230112f98eeeac693d
CarissaZ <92920009+CarissaZ@users.noreply.github.com>
2022-04-07 20:34:58 +0700

    Update f06_ubahstock.py

--- f06_ubahstock.py
+++ f06_ubahstock.py
@@ -1,4 +1,3 @@
-# ga perlu import os krn ga dipake 
 
 # 0 [["id",                 "nama",                "kategori",  "tahun_rilis", "harga",     "stok"],
 # 1 [  "GAME001", "BNMO - Play Along With Crypto",  "Adventure",  "2022"   ,      "100000",    "1"  ]
