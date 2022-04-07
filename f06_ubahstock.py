c24bcb6c3aed08fa0ae8c563c9942c441a4866dc
CarissaZ <92920009+CarissaZ@users.noreply.github.com>
2022-04-07 21:09:06 +0700

    updated f06

--- f06_ubahstock.py
+++ f06_ubahstock.py
@@ -10,18 +10,25 @@
 # game[2][1] = "Dasar Pemrograman"
 
 # parameter dalam fungsi adalah game, karena kitaa pakai array game bukan user
-def stock(game):
+def stok(game):
     id = str(input("Masukkan ID game"))
     stok = int(input("Masukkan jumlah: "))
     found = False # boolean untuk ngecek game ada ga di daftar game
     for data in game: 
         if (data[0] == id): # data[0] menunjukkan kolom ke-0 yaitu kolom id game
             found = True 
+            if stok < 0 :
+                data[5] -= stok
+            elif stok > 0:
+                data[5] += stok
+            elif stok < 0 and (-(stok)) > data[5]:
+                print("Stok game", id, " gagal dikurangi karena stok kurang. Stok sekarang: ", data[5])
             # disini codenya kurang, coba baca ulang spesifikasi. Ada beberapa kasus. :
             # if stok kurang dari stok game(data[5]), output pesan error (coba buat codenya)
             # elif stok < 0, dan > data[5], data[5]+=stok print stok dikurangi (baca di spek) (coba buat codenya)
             # elif stok > 0, data[5]+=stok, print stok ditambah (coba buat codenya)
             data[5] == stok # stok akan berubah sesuai dengan angka yang diinputkan
     if (not(found)):
+        print("Tidak ada game dengan ID tersebut!")
         # coba code disini buat tanganin kasus kalau game ga ada ditemukan di array game (baca spek)
     return game
